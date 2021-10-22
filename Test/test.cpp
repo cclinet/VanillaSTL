711df0d9f7d8415ad5ded8a6f60c443ea14af42f
cclinet <cclinet@outlook.com>
2021-10-22 19:03:46 +0800

    feat: update

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,20 +1,26 @@
-#include "allocator.h"
-#include "array.h"
-#include "array_test.h"
-#include <array>
-#include <type_traits>
+#include <execution>
+#include <iostream>
 #include <vector>
-#include "vector_test.h"
 
 int main() {
-//    vanilla::allocator<int> alloc;
-//    // 演示少见的直接使用成员
-//    static_assert(std::is_same_v<int, decltype(alloc)::value_type>);
-//    int *p = alloc.allocate(1);// 一个 int 的空间
-//    alloc.deallocate(p, 1);    // 而它没了
-//
-//    std::vector<int, vanilla::allocator<int>> v(8);
-//    v.push_back(42);
-//    vanilla::test::array_test();
-    vanilla::test::vector_test();
+    //    vanilla::allocator<int> alloc;
+    //    // 演示少见的直接使用成员
+    //    static_assert(std::is_same_v<int, decltype(alloc)::value_type>);
+    //    int *p = alloc.allocate(1);// 一个 int 的空间
+    //    alloc.deallocate(p, 1);    // 而它没了
+    //
+    //    std::vector<int, vanilla::allocator<int>> v(8);
+    //    v.push_back(42);
+    //    vanilla::test::array_test();
+    //    vanilla::test::vector_test();
+    auto a = std::vector<int>{4, 3, 2, 1};
+    for (auto i : a) {
+        std::cout << i << ' ';
+    }
+    std::cout << std::endl;
+
+    for (auto i : a) {
+        std::cout << i << ' ';
+    }
+    std::cout << std::endl;
 }
--- VanillaSTL/array.h
+++ VanillaSTL/array.h
@@ -5,7 +5,6 @@ namespace vanilla {
     class array {
     public:
         using value_type = T;
-
         using size_type = std::size_t;
         using difference_type = std::ptrdiff_t;
         using reference = value_type &;
@@ -17,15 +16,15 @@ namespace vanilla {
         using reverse_iterator = std::reverse_iterator<iterator>;
         using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
+    private:
         T data[N];
 
+    public:
         constexpr reference at(size_type pos) {
             if (pos >= N)
                 throw std::out_of_range("array::at");
             return data[pos];
         }
-
-
         constexpr const_reference at(size_type pos) const {
             if (pos >= N)
                 throw std::out_of_range("array::at");
@@ -33,17 +32,56 @@ namespace vanilla {
         }
 
         constexpr reference operator[](size_type pos) { return data[pos]; }
-
         constexpr const_reference operator[](size_type pos) const { return data[pos]; }
 
         // std::array<T,N>::begin, std::array<T,N>::cbegin
-        constexpr iterator begin() noexcept { return iterator(data); };
-        constexpr const_iterator begin() const noexcept { return const_iterator(data); };
-        constexpr const_iterator cbegin() const noexcept { return const_iterator(data); };
+        constexpr iterator begin() noexcept { return iterator(&data[0]); };
+        constexpr const_iterator begin() const noexcept { return const_iterator(&data[0]); };
+        constexpr const_iterator cbegin() const noexcept { return const_iterator(&data[0]); };
+
         //std::array<T,N>::end, std::array<T,N>::cend
-        constexpr iterator end() noexcept { return data + N; };
-        constexpr const_iterator end() const noexcept { return data + N; };
-        constexpr const_iterator cend() const noexcept { return data + N; };
+        constexpr iterator end() noexcept { return iterator(&data[N]); };
+        constexpr const_iterator end() const noexcept { return const_iterator(&data[N]); };
+        constexpr const_iterator cend() const noexcept { return const_iterator(&data[N]); };
+
+        //std::array<T,N>::rbegin, std::array<T,N>::crbegin
+        constexpr reverse_iterator rbegin() noexcept { return reverse_iterator(&data[N]); }
+        constexpr const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(&data[N]); }
+        constexpr const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(&data[N]); }
+
+        //std::array<T,N>::rend, std::array<T,N>::crend
+        constexpr reverse_iterator rend() noexcept { return reverse_iterator(&data[0]); }
+        constexpr const_reverse_iterator rend() const noexcept { return const_reverse_iterator(&data[0]); }
+        constexpr const_reverse_iterator crend() const noexcept { return const_reverse_iterator(&data[0]); }
+
+        //std::array<T,N>::empty
+        [[nodiscard]] constexpr bool empty() const noexcept { return N == 0; }
+
+        //std::array<T,N>::size
+        constexpr size_type size() const noexcept { return N; }
+
+        //std::array<T,N>::max_size
+        constexpr size_type max_size() const noexcept { return N; }
+
+        //std::array<T,N>::fill
+        constexpr void fill(const T &value) {
+            for (const_reference tmp : value) {
+                data++ = tmp;
+            }
+        }
+
+        //std::array<T,N>::swap
+        constexpr void swap(array &other) noexcept {
+            std::swap_ranges(&data[0], &data[N], &other.data[0]);
+        }
     };
+
+    template< class T, std::size_t N >
+    constexpr bool operator==( const std::array<T,N>& lhs,
+                              const std::array<T,N>& rhs ){};
+
+    template< class T, std::size_t N >
+    constexpr /* see below */ operator<=>( const std::array<T,N>& lhs,
+                                          const std::array<T,N>& rhs ){};
 }// namespace vanilla
 #endif//VANILLASTL_ARRAY_H
